b31d099ebc9f193ff7cce7e0b0eaf8c19358714c
Greg <gregoryamitten@gmail.com>
2019-06-20 18:45:23 +0100

    Very basic similiarity calculation complete for function bodies

--- dev-playground/ComparisonEngine.js
+++ dev-playground/ComparisonEngine.js
@@ -1,33 +1,58 @@
 const parser = require("@babel/parser");
 const deepDiff = require("deep-diff").diff;
-const esprima = require("esprima")
+// const esprima = require("esprima")
 
 // const FUN_NAMES = ["connect", "collect", "disconnect"];
 const FUN_NAMES = ["one", "two"];
 
 const getDASubtrees = (code) => {
     const AST = parser.parse(code, { sourceType: "module" }).program.body;
-    let funs = {};
+
+    let funASTs = [];
 
     AST.forEach(n => {
-        if (n.type === 'FunctionDeclaration' && FUN_NAMES.includes(n.id.name))
-            funs[n.id.name] = elimateNodeDetails(n);
+        // if (n.type === 'FunctionDeclaration' && FUN_NAMES.includes(n.id.name))
+        //     funs[n.id.name] = elimateNodeDetails(n);
+        if (n.type !== 'EmptyStatement') funASTs.push(elimateNodeDetails(n));
     });
 
-    // return funs;
-    // return deepDiff(funs.one, funs.two)
-    // return calculateNominalDiffValue(deepDiff(funs.one, funs.two));
+    let totalSimiliarity = 0;
+
+    compareFunAST(funASTs).forEach(n => totalSimiliarity += n.similiarity)
+
+    return totalSimiliarity
+    // return calculateSimiliarity(deepDiff(funs[0], funs[1]), countKeys(funs[0]));
 }
 
 const DIFF_VALUES = { D: 10, N: 10, A: 5, E: 2.5 }
 
-const calculateNominalDiffValue = (diff) => {
+const compareFunAST = (funASTs) => {
+    return funASTs[0].body.body.map(n0 => {
+        let bestNode = {
+            similiarity: Number.MAX_VALUE,
+            content: {}
+        };
+
+        funASTs[1].body.body.forEach(n1 => {
+            const similiarity = calculateSimiliarity(n0, n1);
+            if (similiarity < bestNode.similiarity)
+                bestNode = { similiarity, content: n1 }
+        })
+
+        return bestNode;
+    })
+}
+
+const calculateSimiliarity = (node1, node2) => {
     let diffValue = 0;
-    //TODO replace with .reduce
+
+    const diff = deepDiff(node1, node2);
     if (diff) diff.forEach(d => diffValue += DIFF_VALUES[d.kind])
     return diffValue;
 }
 
+const countKeys = (obj) =>
+    JSON.stringify(obj).match(/[^\\]":/g).length
 
 const DETAIL_KEYS = ["name", "identifierName", "start", "end", "loc",
     "trailingComments", "leadingComments", "extra", "value"];
